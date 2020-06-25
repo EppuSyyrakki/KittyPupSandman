4dd3bf5c245ae0fe16f3bcce3aa2bbe5888167c7
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-06-25 15:45:10 +0300

    Added particle effects to coffee
    For fun

---
+++ Assets/Scripts/ParticleTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    private ParticleSystem ps;
+    [Range(0,2)] public float _triggerDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Invoke("TriggerParticles", _triggerDelay);
+    }
+
+    private void TriggerParticles()
+    {
+        ps.Play(false);
+    }
+}
